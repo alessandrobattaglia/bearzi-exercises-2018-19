f2fc5e07ae124f865b4ed64b0ed91f16d5a9eedf
Alessandro <battaglia.alessandro1@gmail.com>
2018-10-05 10:05:17 +0200

    + Added exercise 1

---
+++ 2018 10 06 - Esercizi di base/exercise1.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alessandro Battaglia
+ * Date: 05/10/2018
+ * Time: 09:39
+ *
+ * Multiplication table
+ */
+
+const ROWS = 10;
+const COLS = 10;
+
+for ($r=1; $r<=ROWS; $r++) {
+    for ($c=1; $c<=COLS; $c++)
+        echo '| ' . format($r*$c) . ' ';
+
+    echo "|\n";
+}
+
+/**
+ * Format for nobs
+ *
+ * @param $number
+ * @param int $min
+ * @param string $space
+ * @return string
+ */
+function easyFormat($number, $min = 3, $space = ' ') {
+    if($number<10)
+        return '  ' . $number;
+    elseif($number<100)
+        return ' ' . $number;
+    elseif($number<1000)
+        return $number;
+}
+
+/**
+ * Format for skilled
+ *
+ * @param $number
+ * @param int $min
+ * @param string $space
+ * @return string
+ */
+function format($number, $min = 3, $space = ' ') {
+    $digits = strlen((string)$number);
+
+    $result = [];
+
+    for($i=$digits; $i<$min; $i++)
+        $result[] = $space;
+
+    return join('', $result) . $number;
+}
